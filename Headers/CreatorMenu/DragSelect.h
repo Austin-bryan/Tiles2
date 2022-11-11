c01879e4cc628128c1bd0288db34260adfb04674
Austin Bryan <w.austin.bryan@gmail.com>
2022-11-11 00:26:12 -0500

    Removed unused variables.

--- Headers/CreatorMenu/DragSelect.h
+++ Headers/CreatorMenu/DragSelect.h
@@ -20,27 +20,16 @@ public:
     void TickComponent(float deltaTime, ELevelTick tickType, FActorComponentTickFunction* ThisTickFunction) override;
     void SetBoard(ACreatorBoard* _board) { board = _board; }
     void OnRotate() const;
-
     void ChangeSelectionShape(ESelectionType mode);
 protected:
     UPROPERTY()
         ACreatorBoard* board;
     UPROPERTY()
         ULineBatchComponent* lineBatchComponent;
-    UPROPERTY(EditInstanceOnly)
-        float scale = 56.0f;
     UPROPERTY()
         ASelectionBox* selectionBox;
-    UPROPERTY(EditInstanceOnly)
-        float thickness = 4;
-    UPROPERTY(EditInstanceOnly)
-        float angleMultiplier = 57.25f;
-    UPROPERTY(EditInstanceOnly)
-        int vertexCount = 128;
     TOptional<FVector> anchorPoint;
 private:
     FRotator rotation;
-    bool isDragging;
-
     TUniquePtr<SelectionDrawer> drawer;
 };
