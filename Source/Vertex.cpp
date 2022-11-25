08c5750236fe0d03677d8b496977aa9772ac99cd
Austin Bryan <w.austin.bryan@gmail.com>
2022-11-24 22:51:43 -0500

    Fixed bug with triangle Tiles where vertices wwere incorrect
    - Vertices are transformed into players transform they'll always match
      with scale and rotation
    - Even though intersection points are correct, the vertices aren't being
      moved there properly, perhaps do to their rotation

--- Source/Vertex.cpp
+++ Source/Vertex.cpp
@@ -8,8 +8,8 @@ bool Vertex::IsMerged() const { return hasBeenMerged; }
 
 Vertex  Vertex::PrevVertex()       const { return generator->vertices[(FMath::Abs(vertexIndex - 1 + sideCount) % sideCount)]; }
 Vertex  Vertex::NextVertex()       const { return generator->vertices[(vertexIndex + 1) % sideCount]; }
-FVector Vertex::GetWorldPosition() const { return position + generator->GetOwner()->GetActorLocation(); }
 FVector Vertex::GetLocalPosition() const { return position; }
+FVector Vertex::GetWorldPosition() const { return generator->GetOwner()->GetActorTransform().TransformPosition(position); }
 
 void Vertex::ApplyPosition() { SetPosition(queuedPosition); }
 void Vertex::QueuePosition(const FVector newPosition) { hasBeenMerged = true, queuedPosition = newPosition; }
